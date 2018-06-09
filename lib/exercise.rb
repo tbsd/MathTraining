fabda3ed0db802c80e4d4ccdfcc3a71b08abb1c1
tbsd <tbsd>
2018-06-09 17:56:17 +0300

    Change constructor

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -2,14 +2,14 @@
 class Exercise
   attr_reader :difficulty, :answer, :text
 
-  def initialize(difficulty, answer, text, subject = [])
-    @subject = subject
-    @difficulty = difficulty.to_i
-    @answer = answer.to_i
-    @text = text.to_s
+  def initialize(hash)
+    @subject = hash['subject']
+    @difficulty = hash['difficulty']
+    @answer = hash['answer']
+    @text = hash['text']
   end
 
   def subject_present?(subject)
-    @subject.include?(subject)
+    @subject.include?(subject.strip)
   end
 end
