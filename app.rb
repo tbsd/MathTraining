59ccc2669a2a85867e06ed1e25fe870c6c5bb4a6
subject <subject@cello>
2018-06-09 10:04:05 +0300

    Add get for /general_test

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'pp'
 
 configure do
+  set :exercises, %w[das lll dasdsssssssad] # REMOVE ME mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
   set :root, __dir__
 end
 
@@ -15,4 +17,10 @@ end
 
 get '/theory' do
   erb :theory
+end
+
+get '/general_test' do
+  @exercise = settings.exercises[-1]
+  pp @exercise
+  erb :general_test
 end
