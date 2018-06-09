904b7e987a3b5f92c2b47e0a4822aac22f995b00
tbsd <tbsd>
2018-06-09 20:41:06 +0300

    Add correct attribute

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 # Stores exercise data
 class Exercise
   attr_reader :difficulty, :answer, :text
-
+  attr_accessor :correct
   def initialize(hash)
     @subject = hash['subject']
     @difficulty = hash['difficulty']
     @answer = hash['answer']
     @text = hash['text']
+    @correct = false
   end
 
   def subject_present?(subject)
