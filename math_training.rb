eca0a503238d84bdb1ce92deeddc13ee6ad8eac5
tbsd <tbsd>
2018-06-16 05:02:45 +0300

    Change to using Test class

--- math_training.rb
+++ math_training.rb
@@ -6,11 +6,12 @@ require 'yaml'
 require 'pp'
 require_relative 'lib/exercise'
 require_relative 'lib/yer'
+require_relative 'lib/test'
 
 enable :sessions
 
 configure do
-  set :max_difficulty, 2
+  set :max_dif, 3
   set :exercises_pool, YER.read_exercises
   set :root, __dir__
 end
@@ -28,66 +29,50 @@ get '/theory' do
 end
 
 get '/general_test' do
-  session[:main_list] = []
-  session[:additional] = []
-  session[:past] = []
-  same_difficulty_count = 9
+  main_count = 2
   additional_count = 3
+  dif_count = main_count / settings.max_dif
+  main_list = []
+  additional = []
   shuffled = settings.exercises_pool.shuffle
-  difficulty = 0
-  while difficulty <= settings.max_difficulty
-    same_difficulty = shuffled.select { |e| e.difficulty == difficulty }
-    session[:main_list].concat(same_difficulty.pop(same_difficulty_count))
-    session[:additional].concat(same_difficulty.pop(additional_count))
-    difficulty += 1
+  # If max_dif isn't divisor of main_count
+  main_list.concat(shuffled.pop(main_count % settings.max_dif))
+  dif = 0
+  while dif <= settings.max_dif
+    same_dif = shuffled.find_all { |e| e.difficulty == dif }
+    main_list.concat(same_dif.pop(dif_count))
+    additional.concat(same_dif.pop(additional_count))
+    dif += 1
   end
-  session[:current_exercise] = session[:main_list].shift
+  session[:general_test] = Test.new(main_list.sort, additional.sort)
   @current_number = 1
-  @total_number = session[:main_list].size + 1
-  session[:page] = @current_number
+  @total_number = session[:general_test].total_count
+  # session[:page] = @current_number
   erb :general_test
 end
 
 post '/general_test' do
-  redirect to('/statistics') if session[:current_exercise].nil?
   @warnings = []
   @messages = []
-  session[:past] << session[:current_exercise]
-  # replace if with next_exercise method????????????????????????????????
-  if params['answer'].to_f == session[:current_exercise].answer
-    session[:current_exercise].correct = true
+  redirect to('/statistics') if session[:general_test].ended?
+  if session[:general_test].answer!(params['answer'].to_f)
     @messages << 'Верно!'
-    session[:current_exercise] = session[:main_list].shift
   else
-    session[:current_exercise].correct = false
-    @warnings << 'Ошибка!'
-    new_ex = session[:additional].find do |e|
-      e.difficulty == session[:current_exercise].difficulty
-    end
-    if new_ex.nil?
-      @messages << 'Дополнительных заданий на этом уровне сложности больше нет.'
-      session[:current_exercise] = session[:main_list].shift
-    else
-      @messages << 'Решите дополнительное задание.'
-      session[:current_exercise] = session[:additional].delete(new_ex)
-    end
+    @warnings << 'Ошибка.'
   end
-  return redirect to('/statistics') if session[:current_exercise].nil?
-  @current_number = session[:past].size + 1
-  @total_number = session[:past].size + session[:main_list].size + 1
-  session[:page] = @current_number
+  @current_number = session[:general_test].past_count + 1
+  @total_number = session[:general_test].total_count
+  redirect to('/statistics') if session[:general_test].ended?
+  # session[:page] = @current_number
   erb :general_test
 end
 
 get '/statistics' do
-  return redirect to('/') if session[:past].nil?
-  @total = session[:past].length
-  @correct = session[:past].count(&:correct?)
-  incorrect_array = session[:past].take_while { |e| !e.correct? }
-  @incorrct = incorrect_array.length
-  @mistakes = { :+ => 0, :- => 0, :* => 0, :/ => 0 }
-  @mistakes.each_key do |key|
-    @mistakes[key] = incorrect_array.count { |e| e.subject_present?(key.to_s) }
-  end
+  return redirect to('/practice') if session[:general_test].nil?
+  @total = session[:general_test].total_count
+  @correct = session[:general_test].correct_count
+  @incorrect = @total - @correct
+  @mistakes = session[:general_test].mistake_stats
+  session[:general_test] = nil
   erb :statistics
 end
