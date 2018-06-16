4e20905b043a4f4bfc90bd09f28c721b5bfb31dc
tbsd <tbsd>
2018-06-16 22:45:28 +0300

    Change data storage to server side

--- math_training.rb
+++ math_training.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
+require_relative 'lib/dbio'
+
+user_data = DBIO.read_user_data
+user_data ||= {} # if DBIO.read_user_data returned false (empty file)
+at_exit { DBIO.save_user_data(user_data) }
+
 require 'sinatra'
+require 'sinatra/cookies'
 require 'sinatra/reloader' if development?
-require 'yaml'
 require 'pp'
+require 'securerandom'
 require_relative 'lib/exercise'
-require_relative 'lib/yer'
 require_relative 'lib/test'
 
 enable :sessions
@@ -13,12 +19,21 @@ enable :sessions
 configure do
   set :max_dif, 3
   set :exercises_pool, DBIO.read_exercises
-  set :user_data, DBIO.read_user_data
   set :root, __dir__
 end
 
+before '/practice/*' do
+  id = request.cookies['user_id']
+  if id.nil?
+    new_id = SecureRandom.hex(32)
+    cookies['user_id'] = new_id
+    user_data[new_id] = { 'general_test' => nil, 'rule_test' => nil }
+    id = new_id
+  end
+  session['user_id'] = id
+end
+
 get '/' do
-  pp session
   erb :main
 end
 
@@ -30,7 +45,7 @@ get '/theory' do
   erb :theory
 end
 
-get '/general_test' do
+get '/practice/general_test' do
   main_count = 2
   additional_count = 3
   dif_count = main_count / settings.max_dif
@@ -46,65 +61,72 @@ get '/general_test' do
     additional.concat(same_dif.pop(additional_count))
     dif += 1
   end
-  session[:general_test] = Test.new(main_list.sort, additional.sort)
+  id = session['user_id']
+  user_data[id]['general_test'] = Test.new(main_list.sort, additional.sort)
+  @test = user_data[id]['general_test']
   @current_number = 1
-  @total_number = session[:general_test].total_count
-  # session[:page] = @current_number
+  @total_number = @test.total_count
   erb :general_test
 end
 
-post '/general_test' do
+post '/practice/general_test' do
   @warnings = []
   @messages = []
-  redirect to('/practice') if session[:general_test].nil?
-  redirect to('/statistics?=general_test') if session[:general_test].ended?
-  if session[:general_test].answer!(params['answer'].to_f)
+  id = session['user_id']
+  @test = user_data[id]['general_test']
+  redirect to('/practice') if @test.nil?
+  redirect to('/practice/statistics?test=general_test') if @test.ended?
+  if @test.answer!(params['answer'].to_f)
     @messages << 'Верно!'
   else
     @warnings << 'Ошибка.'
   end
-  @current_number = session[:general_test].past_count + 1
-  @total_number = session[:general_test].total_count
-  redirect to('/statistics?=general_test') if session[:general_test].ended?
-  # session[:page] = @current_number
+  @current_number = @test.past_count + 1
+  @total_number = @test.total_count
+  redirect to('/practice/statistics?test=general_test') if @test.ended?
   erb :general_test
 end
 
-get '/statistics' do
-  test = params[:test]
-  redirect to('/practice') if session[test].nil?
-  @total = session[test].total_count
-  @correct = session[test].correct_count
+get '/practice/statistics' do
+  id = session['user_id']
+  test_name = params[:test]
+  @user = user_data[id]
+  return erb :statistics_choice if test_name.nil?
+  test = user_data[id][test_name]
+  redirect to('/practice/statistics') if test.nil?
+  @total = test.total_count
+  @correct = test.correct_count
   @incorrect = @total - @correct
-  @mistakes = session[test].mistake_stats
-  # session[test] = nil
+  @mistakes = test.mistake_stats
   erb :statistics
 end
 
-get '/rule_choice' do
+get '/practice/rule_choice' do
   erb :rule_choice
 end
 
-get '/rule_test' do
-  redirect to('/rule_choice') unless params.key?(:rule)
+get '/practice/rule_test' do
+  id = session['user_id']
+  redirect to('/practice/rule_choice') unless params.key?(:rule)
   same_rule = settings.exercises_pool.find_all { |e| e.subject?(params[:rule]) }
-  redirect to('/rule_choice') if same_rule.empty? # No such rule
-  session[:rule_test] = Test.new(same_rule, [])
+  redirect to('/practice/rule_choice') if same_rule.empty? # No such rule
+  user_data[id]['rule_test'] = Test.new(same_rule.shuffle, [])
+  @test = user_data[id]['rule_test']
   erb :rule_test
 end
 
-post '/rule_test' do
+post '/practice/rule_test' do
+  id = session['user_id']
+  @test = user_data[id]['rule_test']
   @warnings = []
   @messages = []
-  redirect to('/rule_choice') if session[:rule_test].nil?
-  redirect to('/statistics?=rule_test') if session[:rule_test].ended?
-  if session[:rule_test].answer!(params['answer'].to_f)
+  redirect to('/practice/rule_choice') if @test.nil?
+  redirect to('/practice/statistics?test=rule_test') if @test.ended?
+  if @test.answer!(params['answer'].to_f)
     @messages << 'Верно!'
   else
     @warnings << 'Ошибка.'
   end
-  redirect to('/statistics?=rule_test') if session[:rule_test].ended?
-  # session[:page] = @current_number
+  redirect to('/practice/statistics?test=rule_test') if @test.ended?
   erb :rule_test
 end
-
