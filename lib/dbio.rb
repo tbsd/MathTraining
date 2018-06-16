81b9fab53734a25f26fa86c14ccd53d0dbdf8ebd
tbsd <tbsd>
2018-06-16 23:30:29 +0300

    Add no user_data file handling

--- lib/dbio.rb
+++ lib/dbio.rb
@@ -14,11 +14,15 @@ module DBIO
   end
 
   def self.read_user_data
-    YAML.load_file('data/user_data.yaml')
+    path = 'data/user_data.yaml'
+    if File.exist?(path)
+      YAML.load_file(path)
+    else
+      false
+    end
   end
 
   def self.save_user_data(data)
-    # File.open('data/user_data.yaml') { |f| f.write(data.to_yaml) }
-    File.write('data/user_data.yaml', data.to_yaml)
+    File.open('data/user_data.yaml', 'w') { |f| f.write(data.to_yaml) }
   end
 end
