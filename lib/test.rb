8c4c29bdff068ce2e45f62f2ae3da2bc226925d1
tbsd <tbsd>
2018-06-29 17:53:14 +0300

    Rewrite Test class implementation, so there are array and int counter instead of two arrays now

--- lib/test.rb
+++ lib/test.rb
@@ -2,46 +2,46 @@
 
 # Stores test data
 class Test
-  attr_reader :current
-
   def initialize(main_list, additional_list = [])
     @main_list = main_list
     @additional_list = additional_list
-    @past = []
-    @current = main_list.shift
+    @cur_pos = 0
     @ended = false
   end
 
   def answer!(answer)
     raise 'Test is over' if @ended
-    @current.make_correct! if @current.answer == answer
-    @past << @current
-    @current = next_exercise
-    @ended = true if @current.nil?
-    @past.last.correct?
+    @main_list[@cur_pos].make_correct! if @main_list[@cur_pos].answer == answer
+    next_exercise
+    @ended = true if @cur_pos >= @main_list.size
+    @main_list[@cur_pos - 1].correct?
   end
 
   def expected_count
-    count = @past.size + @main_list.size
-    count += 1 unless @current.nil?
-    count
+    @main_list.size
   end
 
   def abort!
     @ended = true
   end
 
+  def current
+    @main_list[@cur_pos]
+  end
+
   def past_count
-    @past.size
+    @cur_pos
   end
 
   def correct_count
-    @past.count(&:correct?)
+    past = @main_list.first(@cur_pos)
+    past.count(&:correct?)
   end
 
   def mistake_stats
     mistakes = { :+ => 0, :- => 0, :* => 0, :/ => 0 }
-    incorrect = @past.find_all { |e| !e.correct? }
+    past = @main_list.first(@cur_pos)
+    incorrect = past.find_all { |e| !e.correct? }
     mistakes.each_key do |key|
       mistakes[key] = incorrect.count { |e| e.subject?(key.to_s) }
     end
@@ -55,12 +55,11 @@ class Test
 
   def next_exercise
     add_ex = @additional_list.find_index do |e|
-      e.difficulty == @current.difficulty
+      e.difficulty == @main_list[@cur_pos].difficulty
     end
-    if @current.correct? || add_ex.nil?
-      @main_list.shift
-    else
-      @additional_list.delete_at(add_ex)
+    unless @main_list[@cur_pos].correct? || add_ex.nil?
+      @main_list.insert(@cur_pos + 1, @additional_list.delete_at(add_ex))
     end
+    @cur_pos += 1
   end
 end
